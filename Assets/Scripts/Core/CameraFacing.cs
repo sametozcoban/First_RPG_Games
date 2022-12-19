3bc4d70370a07ce657e2d86ed2fbdd4321eadcc5
Samet <ozcobansamet@gmail.com>
2022-12-19 09:45:55 +0300

    no message

---
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class CameraFacing : MonoBehaviour
+    {
+        private void Update()
+        {
+            transform.forward = Camera.main.transform.forward; //Hazırlanan Canvas içerisinde ki TEXT in  daima kameraya doğru bakmasını sağlayacak.
+        }
+    }
+}
